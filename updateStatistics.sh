b5beefc7a51b3c8680ec5de0db12205450a9d70b
lilients <svantje.lilienthal@fu-berlin.de>
2015-09-04 14:12:13 +0200

    script for statistics added

---
+++ updateStatistics.sh
@@ -0,0 +1,4 @@
+cd /data/omp/langsci-test/lsp-scripts/webstats/
+python extractaw.py
+mv /data/omp/langsci-test/lsp-scripts/webstats/*.png /data/omp/langsci-test/plugins/generic/bookPage/img/
+mv /data/omp/langsci-test/lsp-scripts/webstats/*.svg /data/omp/langsci-test/plugins/generic/bookPage/img/
